695bd0d586546523fa519d39842e0dd5e6949759
marcof <m.fagiani85@gmail.com>
2021-01-21 00:24:28 +0100

    add plot with predicted values

--- app/run.py
+++ app/run.py
@@ -10,7 +10,7 @@ from flask import render_template, request, jsonify
 import plotly
 
 from data_retriever.retriever import get_daily_historical
-from utils.plot_factory import plot_historical
+from utils.plot_factory import plot_historical_with_predicted_data
 from utils.defaults import DEFAULT_SYMBOLS
 from models.train_regressor import prepare_data, clean_data, load_model
 
@@ -50,9 +50,9 @@ def index():
         regression_input = regression_input[-1, :].reshape(1, -1)
 
         # compute predictions
-        regression_outputs = model.predict(regression_input)[0]
+        regression_outputs = model.predict(regression_input)[-1]
 
-        current_price = data['Adj Close'].iloc[0]
+        current_price = data['Adj Close'].iloc[-1]
         regression_pct_returns = 100 * (regression_outputs - current_price) / current_price
         regression_pct_returns = np.round(regression_pct_returns, decimals=2)
 
@@ -61,8 +61,29 @@ def index():
         regression_result_14d.update({symbol: regression_pct_returns[2]})
         regression_result_28d.update({symbol: regression_pct_returns[3]})
 
+        # create dataframe with predicted data
+        predicted_data = pd.DataFrame(index=[data.index[-1]],
+                                      data=[[data.iloc[-1]['Adj Close']]*4],
+                                      columns=['Adj Close - 1d_prediction', 'Adj Close - 7d_prediction', 'Adj Close - 14d_prediction', 'Adj Close - 28d_prediction'])
+        # append 1day prediction
+        predicted_data = predicted_data.append(pd.DataFrame(index=[predicted_data.index[0] + pd.Timedelta(days=1)],
+                                                            data=[regression_outputs[0]],
+                                                            columns=['Adj Close - 1d_prediction']))
+        # append 7day prediction
+        predicted_data = predicted_data.append(pd.DataFrame(index=[predicted_data.index[0] + pd.Timedelta(days=7)],
+                                                            data=[regression_outputs[1]],
+                                                            columns=['Adj Close - 7d_prediction']))
+        # append 14day prediction
+        predicted_data = predicted_data.append(pd.DataFrame(index=[predicted_data.index[0] + pd.Timedelta(days=14)],
+                                                            data=[regression_outputs[2]],
+                                                            columns=['Adj Close - 14d_prediction']))
+        # append 28day prediction
+        predicted_data = predicted_data.append(pd.DataFrame(index=[predicted_data.index[0] + pd.Timedelta(days=28)],
+                                                            data=[regression_outputs[3]],
+                                                            columns=['Adj Close - 28d_prediction']))
+
         # plot data
-        fig = plot_historical(symbol, data, return_fig=True)
+        fig = plot_historical_with_predicted_data(symbol, data, predicted_data, return_fig=True)
         figures.append(fig)
 
     # encode plotly graphs in JSON
--- utils/plot_factory.py
+++ utils/plot_factory.py
@@ -46,5 +46,59 @@ def plot_historical(symbol_name: str, data: pd.DataFrame, open_file=False, retur
     poff(fig, filename=plot_filename, auto_open=open_file)
 
 
+def plot_historical_with_predicted_data(symbol_name: str, data: pd.DataFrame, predicted_data: pd.DataFrame,
+                                        open_file=False, return_fig=False):
+    """ plot historical data from data retriever """
+
+    traces = list()
+    plot_title = f"Stock: {symbol_name}"
+    plot_filename = f"stock_{symbol_name}.html"
+
+    for col in data.columns:
+
+        traces.append(go.Scatter(
+            x=data[col].index,
+            y=data[col].values,
+            name=col,
+            showlegend=True)
+        )
+
+    for col in predicted_data.columns:
+
+        traces.append(go.Scatter(
+            x=predicted_data[col].dropna().index,
+            y=predicted_data[col].dropna().values,
+            name=col,
+            line=dict(color='red', dash='dot'),
+            showlegend=True)
+        )
+
+    fig = go.Figure(traces)
+
+    fig.update_layout(
+        title_text=plot_title,
+        xaxis_title="Date",
+        yaxis_title="Price ($)")
+        # legend_title="Legend Title")
+
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+
+    if return_fig:
+        return fig
+
+    poff(fig, filename=plot_filename, auto_open=open_file)
+
+
 def plot_multiple_historical(data):
     raise NotImplementedError
