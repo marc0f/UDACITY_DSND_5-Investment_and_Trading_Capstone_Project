4eade064360ec4aad0d5caddd18c3bb5db95bc0f
marcof <m.fagiani85@gmail.com>
2021-02-15 00:31:51 +0100

    plot features

--- data_retriever/data_analysis.py
+++ data_retriever/data_analysis.py
@@ -3,6 +3,7 @@ from data_retriever.retriever import get_daily_historical
 from utils.plot_factory import plot_historical
 
 from utils.defaults import DEFAULT_SYMBOLS
+from utils.features import sma, ema, vwap
 
 
 if __name__ == '__main__':
@@ -15,6 +16,22 @@ if __name__ == '__main__':
 
         # get OHLC data
         data = get_daily_historical(symbol, start_date, end_date)
+
+        # compute features
+        targets = data['Adj Close']
+
+        # compute features
+        data['sma_20'] = sma(targets, periods=20)
+        data['sma_100'] = sma(targets, periods=100)
+        data['ema_10'] = ema(targets, periods=10)
+        data['ema_50'] = ema(targets, periods=50)
+        data['vwap_20'] = vwap(price_data=targets, volume_data=data['Volume'], periods=20)
+        data['vwap_100'] = vwap(price_data=targets, volume_data=data['Volume'], periods=100)
+
+        for lag in [1, 7, 14, 28]:
+            data[f"diff_{lag}"] = targets.diff(periods=lag)
+
+        data = data.dropna()  # drop rows with at least 1 nans
 
         # plot data
         plot_historical(symbol, data, open_file=True)
